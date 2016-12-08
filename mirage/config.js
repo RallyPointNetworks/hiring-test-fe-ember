f04162c63830888f369af134c88e0e4d26593340
Andre Behrens <andre@rallypoint.com>
2016-12-08 13:51:58 -0500

    Add mirage depenndencies and back end support.

---
+++ mirage/config.js
@@ -0,0 +1,40 @@
+export default function() {
+
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Shorthand cheatsheet:
+
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+
+    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
+  */
+  this.namespace = 'api';
+
+  this.get('/customers', function(db, request) {
+    let { query } = request.queryParams;
+    if (query) {
+      let filterRegEx = new RegExp(query.toUpperCase(), 'gi');
+      return db.customers.filter((customer) => {
+        let condensedForm = `${customer.firstName}${customer.lastName}${customer.dueMonthly}`;
+        filterRegEx.test(condensedForm);
+      });
+    } else {
+      return db.customers;
+    }
+  });
+}
